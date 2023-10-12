9d62fe0deeba65927bc80b7531a188232d75e4e5
SiThuTun22 <sithu1492004@gmail.com>
2023-10-13 00:50:21 +0630

    plus challenge #3

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
+]);
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+gameEvents.delete(64);
+console.log(
+  'An event happended, on average, every ' + 90 / gameEvents.size + ' minutes'
+);
+for (const [key, value] of gameEvents) {
+  console.log(
+    (key < 45 ? '[FIRST HALF] ' : '[SECOND HALF] ') + key + ': ' + value
+  );
+}
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
